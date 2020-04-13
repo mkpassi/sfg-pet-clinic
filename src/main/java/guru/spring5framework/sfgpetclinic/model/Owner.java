92a4efb9917f735f2cf7d3fc3d6fe6c3cb4d97e7
miteshpassi <mitesh.passi@gmail.com>
2020-04-13 12:12:49 +0530

    Pojo Added

---
+++ src/main/java/guru/spring5framework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.spring5framework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/spring5framework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.spring5framework.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
