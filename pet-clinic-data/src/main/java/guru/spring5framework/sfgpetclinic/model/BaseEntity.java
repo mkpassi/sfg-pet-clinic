7458c959991c0133a731e4767ee77aa5eb13e1aa
miteshpassi <mitesh.passi@gmail.com>
2020-04-15 12:44:52 +0530

    Refactored to modules. closes #2

---
+++ pet-clinic-data/src/main/java/guru/spring5framework/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package guru.spring5framework.sfgpetclinic.model;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ */
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
