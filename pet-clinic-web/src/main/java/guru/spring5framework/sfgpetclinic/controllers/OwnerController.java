fdfbe0d45fe556cea103937c2301d69d4b29bbbf
miteshpassi <mitesh.passi@gmail.com>
2020-04-18 18:46:09 +0530

    Added BootStrap Data. Closes #6

--- pet-clinic-web/src/main/java/guru/spring5framework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/spring5framework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package guru.spring5framework.sfgpetclinic.controllers;
 
+import guru.spring5framework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners","/owners/index","/owners/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
