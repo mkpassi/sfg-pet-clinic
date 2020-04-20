7f0ca8db3c6b56e1b342c7b281f8965ba5fa6629
miteshpassi <mitesh.passi@gmail.com>
2020-04-20 21:44:30 +0530

    Added layout index page.

--- pet-clinic-data/src/main/java/guru/spring5framework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/spring5framework/sfgpetclinic/model/Owner.java
@@ -2,9 +2,39 @@ package guru.spring5framework.sfgpetclinic.model;
 
 import java.util.Set;
 
+/**
+ * Owner Class
+ */
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
@@ -12,5 +42,16 @@ public class Owner extends Person {
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Owner{");
+        sb.append("address='").append(address).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append(", telephone='").append(telephone).append('\'');
+        sb.append(", pets=").append(pets);
+        sb.append('}');
+        return sb.toString();
     }
 }
