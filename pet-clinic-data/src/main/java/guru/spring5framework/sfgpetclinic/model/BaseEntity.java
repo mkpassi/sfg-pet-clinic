71be08685b4f3ac999f7155c7a60a13361d5b16d
miteshpassi <mitesh.passi@gmail.com>
2020-04-23 22:55:04 +0530

    IMplemented Base entity

--- pet-clinic-data/src/main/java/guru/spring5framework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/spring5framework/sfgpetclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package guru.spring5framework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Base Entity
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id // this is the id Value
+    //Generation Type strategy TABLE|SEQUENCE|IDENTITY|AUTO
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
